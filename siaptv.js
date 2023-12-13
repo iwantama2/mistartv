fbd213c41d602ea63a47daca92bcdab93ad26a6c
Siap Ngoding <46395434+iwantama2@users.noreply.github.com>
2023-12-13 15:09:42 +0700

    Create siaptv.js

---
+++ siaptv.js
@@ -0,0 +1,160 @@
+var _0x12c9c8 = function () {
+  var _0x4ed447 = true;
+  return function (_0x594b77, _0x28f230) {
+    var _0x4602d9 = _0x4ed447 ? function () {
+      if (_0x28f230) {
+        var _0x1ac0f6 = _0x28f230.apply(_0x594b77, arguments);
+        _0x28f230 = null;
+        return _0x1ac0f6;
+      }
+    } : function () {};
+    _0x4ed447 = false;
+    return _0x4602d9;
+  };
+}();
+var _0x1b6dc1 = _0x12c9c8(this, function () {
+  return _0x1b6dc1.toString().search("(((.+)+)+)+$").toString().constructor(_0x1b6dc1).search("(((.+)+)+)+$");
+});
+_0x1b6dc1();
+var _0x17c687 = function () {
+  var _0x378779 = true;
+  return function (_0xd8c536, _0x4d4003) {
+    var _0x2e64b0 = _0x378779 ? function () {
+      if (_0x4d4003) {
+        var _0x3d639a = _0x4d4003.apply(_0xd8c536, arguments);
+        _0x4d4003 = null;
+        return _0x3d639a;
+      }
+    } : function () {};
+    _0x378779 = false;
+    return _0x2e64b0;
+  };
+}();
+(function () {
+  _0x17c687(this, function () {
+    var _0x5e7f4e = new RegExp('function *\( *\)');
+    var _0x114e2b = new RegExp('\+\+ *(?:[a-zA-Z_$][0-9a-zA-Z_$]*)', 'i');
+    var _0x2b04bf = _0xb6239d("init");
+    if (!_0x5e7f4e.test(_0x2b04bf + "chain") || !_0x114e2b.test(_0x2b04bf + "input")) {
+      _0x2b04bf('0');
+    } else {
+      _0xb6239d();
+    }
+  })();
+})();
+var _0x9af875 = function () {
+  var _0x3ccf22 = true;
+  return function (_0x3f28bf, _0x1acb90) {
+    var _0x31d310 = _0x3ccf22 ? function () {
+      if (_0x1acb90) {
+        var _0x194efb = _0x1acb90.apply(_0x3f28bf, arguments);
+        _0x1acb90 = null;
+        return _0x194efb;
+      }
+    } : function () {};
+    _0x3ccf22 = false;
+    return _0x31d310;
+  };
+}();
+var _0x42e03b = _0x9af875(this, function () {
+  var _0x3182fa;
+  try {
+    var _0x76a4d1 = Function("return (function() {}.constructor(\"return this\")( ));");
+    _0x3182fa = _0x76a4d1();
+  } catch (_0xb458e3) {
+    _0x3182fa = window;
+  }
+  var _0x575934 = _0x3182fa.console = _0x3182fa.console || {};
+  var _0x229f83 = ["log", "warn", "info", "error", "exception", "table", "trace"];
+  for (var _0x5c2633 = 0; _0x5c2633 < _0x229f83.length; _0x5c2633++) {
+    var _0x28f969 = 0;
+    var _0x237e88 = _0x9af875.constructor.prototype.bind(_0x9af875);
+    var _0x2c6c31 = _0x229f83[_0x5c2633];
+    var _0x587672 = _0x575934[_0x2c6c31] || _0x237e88;
+    _0x237e88.__proto__ = _0x9af875.bind(_0x9af875);
+    _0x237e88.toString = _0x587672.toString.bind(_0x587672);
+    _0x575934[_0x2c6c31] = _0x237e88;
+  }
+});
+_0x42e03b();
+function indosiar() {
+  document.getElementById('tv').innerHTML = '<iframe src="https://live.sportsnews.id/p/ply-tv.html?file=https://habotv.github.io/live/indosia.m3u8" style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;allow-forms allow-pointer-lock allow-same-origin-allow-popups-allow-scripts allow-top-navigation;" allowfullscreen="true"></iframe>';
+}
+function sctv() {
+  document.getElementById('tv').innerHTML = '<iframe src="https://live.sportsnews.id/p/ply-tv.html?file=https://habotv.github.io/live/sctp.m3u8" style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;allow-forms allow-pointer-lock allow-same-origin-allow-popups-allow-scripts allow-top-navigation;" allowfullscreen="true"></iframe>';
+}
+function moji() {
+  document.getElementById('tv').innerHTML = '<iframe src="https://live.sportsnews.id/p/ply-tv.html?file=https://etslive-2-vidio-com.akamaized.net/exp=1689246637~acl=/hls-p/ingest_206_*/*~hmac=e273b10f2fabd03cca3a56119b5f92da92f75da9dc879e8390c5a052a00c7af0/hls-p/ingest_206_480p/index.m3u8" style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;allow-forms allow-pointer-lock allow-same-origin-allow-popups-allow-scripts allow-top-navigation;" allowfullscreen="true"></iframe>';
+}
+function mentari() {
+  document.getElementById('tv').innerHTML = '<iframe src="https://live.sportsnews.id/p/ply-tv.html?file=https://habotv.github.io/live/mentari.m3u8" style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;allow-forms allow-pointer-lock allow-same-origin-allow-popups-allow-scripts allow-top-navigation;" allowfullscreen="true"></iframe>';
+}
+function sony1hls() {
+  document.getElementById('tv').innerHTML = '<iframe src="https://live.sportsnews.id/p/ply-tv.html?file=https://dai.google.com/linear/hls/event/6WhVNGKTRXyu588zZv1Lkg/master.m3u8" style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;allow-forms allow-pointer-lock allow-same-origin-allow-popups-allow-scripts allow-top-navigation;" allowfullscreen="true"></iframe>';
+}
+function sony2hls() {
+  document.getElementById('tv').innerHTML = '<iframe src="https://live.sportsnews.id/p/ply-tv.html?file=https://dai.google.com/linear/hls/event/LK-ik89MQIi_pWBbg74KNQ/master.m3u8" style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;allow-forms allow-pointer-lock allow-same-origin-allow-popups-allow-scripts allow-top-navigation;" allowfullscreen="true"></iframe>';
+}
+function sony3hls() {
+  document.getElementById('tv').innerHTML = '<iframe src="https://live.sportsnews.id/p/ply-tv.html?file=https://dai.google.com/linear/hls/event/BCOFZq1JQjq12fmaO6lAAA/master.m3u8" style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;allow-forms allow-pointer-lock allow-same-origin-allow-popups-allow-scripts allow-top-navigation;" allowfullscreen="true"></iframe>';
+}
+function sony4hls() {
+  document.getElementById('tv').innerHTML = '<iframe src="https://live.sportsnews.id/p/ply-tv.html?file=https://dai.google.com/linear/hls/event/smYybI_JToWaHzwoxSE9qA/master.m3u8" style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;allow-forms allow-pointer-lock allow-same-origin-allow-popups-allow-scripts allow-top-navigation;" allowfullscreen="true"></iframe>';
+}
+function sony5hls() {
+  document.getElementById('tv').innerHTML = '<iframe src="https://live.sportsnews.id/p/ply-tv.html?file=https://dai.google.com/linear/hls/event/r-eLp41YTHWTagvQSXFtAQ/master.m3u8" style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;allow-forms allow-pointer-lock allow-same-origin-allow-popups-allow-scripts allow-top-navigation;" allowfullscreen="true"></iframe>';
+}
+(function () {
+  var _0x5ac9e0 = function () {
+    var _0x420fab;
+    try {
+      _0x420fab = Function("return (function() {}.constructor(\"return this\")( ));")();
+    } catch (_0x243afe) {
+      _0x420fab = window;
+    }
+    return _0x420fab;
+  };
+  var _0xd89c0a = _0x5ac9e0();
+  _0xd89c0a.setInterval(_0xb6239d, 4000);
+})();
+function zeebioskop() {
+  document.getElementById('tv').innerHTML = '<iframe src="https://live.sportsnews.id/p/zeebioskop.html" style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;allow-forms allow-pointer-lock allow-same-origin-allow-popups-allow-scripts allow-top-navigation;" allowfullscreen="true"></iframe>';
+}
+function axn() {
+  document.getElementById('tv').innerHTML = '<iframe src="https://live.sportsnews.id/p/axn.html" style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;allow-forms allow-pointer-lock allow-same-origin-allow-popups-allow-scripts allow-top-navigation;" allowfullscreen="true"></iframe>';
+}
+function one() {
+  document.getElementById('tv').innerHTML = '<iframe src="https://live.sportsnews.id/p/one.html" style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;allow-forms allow-pointer-lock allow-same-origin-allow-popups-allow-scripts allow-top-navigation;" allowfullscreen="true"></iframe>';
+}
+function animax() {
+  document.getElementById('tv').innerHTML = '<iframe src="https://live.sportsnews.id/p/animax.html" style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;allow-forms allow-pointer-lock allow-same-origin-allow-popups-allow-scripts allow-top-navigation;" allowfullscreen="true"></iframe>';
+}
+function nickjr() {
+  document.getElementById('tv').innerHTML = '<iframe src="https://live.sportsnews.id/p/nickjr.html" style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;allow-forms allow-pointer-lock allow-same-origin-allow-popups-allow-scripts allow-top-navigation;" allowfullscreen="true"></iframe>';
+}
+function _0xb6239d(_0x338269) {
+  function _0x424472(_0x20d9ce) {
+    if (typeof _0x20d9ce === "string") {
+      return function (_0x76e293) {}.constructor("while (true) {}").apply("counter");
+    } else {
+      if (('' + _0x20d9ce / _0x20d9ce).length !== 1 || _0x20d9ce % 20 === 0) {
+        (function () {
+          false;
+          return true;
+        }).constructor("debugger").call("action");
+      } else {
+        (function () {
+          return false;
+        }).constructor("debugger").apply("stateObject");
+      }
+    }
+    _0x424472(++_0x20d9ce);
+  }
+  try {
+    if (_0x338269) {
+      return _0x424472;
+    } else {
+      _0x424472(0);
+    }
+  } catch (_0x5b0702) {}
+}
